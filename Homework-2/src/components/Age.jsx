f3a32e0fa82e7e344243f783f39e13f115788390
KarloBrsak <kbrsak@hotmail.com>
2024-03-19 18:35:59 +0100

    Add paragraph for younger users.

--- Homework-2/src/components/Age.jsx
+++ Homework-2/src/components/Age.jsx
@@ -9,13 +9,23 @@ export const Age = props => {
         </li>
       );
     }
-    return null;
+  });
+  const pomladi = props.lugje.map((covek, i) => {
+    if (covek.godini < 18) {
+      return (
+        <li key={i}>
+          {covek.ime} {covek.prezime}
+        </li>
+      );
+    }
   });
 
   return (
     <div>
       <p>Lica postari od 18:</p>
       <ul>{postari}</ul>
+      <p>Lica pomladi od 18:</p>
+      <ul>{pomladi}</ul>
     </div>
   );
 };
